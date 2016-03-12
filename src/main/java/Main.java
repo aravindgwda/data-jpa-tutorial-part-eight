ae4dbaa12a1253c64b1d7bcfec7d19521d24b3db
aravindgwda <aravindgwda@gmail.com>
2016-03-12 16:17:17 +0530

    Removing the generated file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        String webappDirLocation = "webapp/";
+        String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
 
         //The port that we should run on can be set into an environment variable
